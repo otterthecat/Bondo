61199af2678743b52ea5503403ff4cc834ab6195
Otter <otc@otterthecat.com>
2015-08-12 22:53:45 -0500

    initial commit

---
+++ gulp/config/options.js
@@ -0,0 +1,40 @@
+var complexityOpts = {
+    errorsOnly : false,
+    cyclomatic : 3,
+    halstead : 10,
+    maintainability : 90,
+    trycatch : true
+};
+
+module.exports = {
+
+    browserSync: {
+        proxy: 'localhost/github/stylus-stuff/public'
+    },
+
+    plato: {
+        dest: './metrics/report',
+        complexity: complexityOpts,
+        jshint: {
+            options: {
+                strict: true
+            }
+        }
+    },
+
+    complexity: complexityOpts,
+
+    istanbul: {
+        dir: './metrics/coverage',
+        reporters: ['text-summary', 'html']
+    },
+
+    mocha: {
+        reporter: 'nyan'
+    },
+
+    size: {
+        showFiles: true,
+        title: 'File Size: '
+    }
+};
---
+++ gulp/config/sources.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    'js': ['./dev/javascripts/**/*.js'],
+
+    'app': ['./dev/javascripts/**/*.js', './dev/styles/**/*.styl'],
+
+    'module': ['./dev/javascripts/main.js'],
+
+    'css': ['./dev/styles/main.styl'],
+};
